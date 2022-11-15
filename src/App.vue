f39cff36e5835e4fee53393ec158d85f57e6ae52
davlatmir <davlatmirshamsherov@gmail.com>
2022-11-16 03:15:44 +0500

    v-main-wraper add

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,20 @@
   <div id="app">
     <p>Hello Vue</p>
   </div>
+  <v-main-wrapper></v-main-wrapper>
 </template>
 
 <script>
+import vMainWrapper from './components/v-main-wrapper.vue'
 export default {
   name: 'app',
-
+  components: { vMainWrapper },
 }
 </script>
 
 <style>
-
-</style>
-<style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="v-main-wrapper.vue">
+    <p>{{title}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'v-main-wrapper.vue',
+    data() {
+        return {
+            title: 'Main wrapper'
+        }
+    },
+}
+</script>
+
+<style>
+.v-main-wrapper.vue{
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+</style>
