36c13e6aac2e8964cc151f79a0a5d05ce8a662e8
davlatmir <davlatmirshamsherov@gmail.com>
2022-11-16 02:42:27 +0500

    #add style

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  
+  name: 'app',
 
 }
 </script>
@@ -15,5 +15,12 @@ export default {
 
 </style>
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 160px;
+}
 </style>
